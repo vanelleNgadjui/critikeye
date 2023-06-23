96d60e71826fb9bb7ae80dafc77642063cc48cb2
vanelleNgadjui <vanellejob@gmail.com>
2023-06-23 14:51:49 +0200

    tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,8 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: ['./critikeye_app/static/css/**/*.css', './critikeye_app/templates/**/*.html'],
+  // Autres options de configuration
+
 }
 
